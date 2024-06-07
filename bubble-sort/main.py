c08b5760b9dc642bea9a4a722afe3852ae23732a
Kevin Dela Paz <kevindelapaz@Kevins-Air.attlocal.net>
2024-06-07 16:10:42 -0500

    did binary search and bubble sort

---
+++ bubble-sort/main.py
@@ -0,0 +1,16 @@
+def bubble_sort(nums):
+    pass
+
+
+# Procedure bubble_sort(nums):
+#     Set swapping to True
+#     Set end to the length of nums
+#     While swapping is True:
+#         Set swapping to False
+#         For i from the 2nd element to end:
+#             If the (i-1)th element of nums is greater than the ith element:
+#                 Swap the (i-1)th element and the ith element of nums
+#                 Set swapping to True
+#         Reduce end by one
+#     Return nums
+# End Procedure
