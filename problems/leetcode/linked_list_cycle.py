68e3ede0aa97dcb0fd1e3d0f724a5ce7dddfa2f5
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-07-09 21:39:37 -0500

    fixed

--- problems/leetcode/linked_list_cycle.py
+++ problems/leetcode/linked_list_cycle.py
@@ -1,3 +1,9 @@
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 class Solution(object):
     def hasCycle(self, head):
         # visited_nodes = {}
