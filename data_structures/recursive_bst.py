3ddbac5278a880fdf5528801642e101ec36fa3ea
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-07-25 08:46:52 -0500

    added contains method for bst

--- data_structures/recursive_bst.py
+++ data_structures/recursive_bst.py
@@ -9,6 +9,19 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
+    def __r_contains(self, current_node, value):
+        if current_node is None:
+            return False
+        if current_node.value == value:
+            return True
+        if value > current_node.value:
+            return self.__r_contains(current_node.right, value)
+        else:
+            return self.__r_contains(current_node.left, value)
+
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value)
+
     def __r_insert(self, current_node, value):
         if current_node == None:
             return Node(value)
