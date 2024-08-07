68885dab79273438ee1d38ea37f61cc4efe2a99c
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-08-07 17:17:45 -0500

    implemented graph data structure

---
+++ data_structures/graph.py
@@ -0,0 +1,42 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def print_graph(self):
+        v_list = []
+        for vertex in self.adj_list:
+            v_list.append(vertex)
+        v_list.sort()
+        for v in v_list:
+            print(v, ':', self.adj_list[v])
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+
+    def remove_vertex(self, vertex):
+        if vertex not in self.adj_list:
+            return False
+        for vert in self.adj_list[vertex]:
+            self.adj_list[vert].remove(vertex)
+        del self.adj_list[vertex]
+        return True
