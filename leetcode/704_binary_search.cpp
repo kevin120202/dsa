e6b7e6743f4b9f6e18185191dd06f78436ebe167
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-09-30 21:33:58 -0500

    daily lc

--- leetcode/704_binary_search.cpp
+++ leetcode/704_binary_search.cpp
@@ -1,24 +1,24 @@
-#include <vector>
+// #include <vector>
 
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size() - 1;
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int l = 0;
+//         int r = nums.size() - 1;
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            else if (target < nums[mid]) {
-                r = mid - 1;
-            }
-            else if (target > nums[mid]) {
-                l = mid + 1;
-            }
-        }
+//         while (l <= r) {
+//             int mid = l + (r - l) / 2;
+//             if (nums[mid] == target) {
+//                 return mid;
+//             }
+//             else if (target < nums[mid]) {
+//                 r = mid - 1;
+//             }
+//             else if (target > nums[mid]) {
+//                 l = mid + 1;
+//             }
+//         }
 
-        return -1;
-    }
-};
+//         return -1;
+//     }
+// };
